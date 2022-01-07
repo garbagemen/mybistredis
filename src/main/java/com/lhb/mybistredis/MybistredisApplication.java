c0b50cffae417bb47c5d56606b87817ca0bea5ff
86173 <2408671088@qq.com>
2022-01-07 16:23:35 +0800

    丢丢丢

---
+++ src/main/java/com/lhb/mybistredis/MybistredisApplication.java
@@ -0,0 +1,13 @@
+package com.lhb.mybistredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybistredisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybistredisApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lhb/mybistredis/com/lhb/pojo/Stuent.java
@@ -0,0 +1,8 @@
+package com.lhb.mybistredis.com.lhb.pojo;
+
+/**
+ * @author 刘贺博
+ * @version 创建时间:2022/1/6 18:55
+ */
+public class Stuent {
+}
